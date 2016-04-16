9c822b67ec732ea443693bec059043787a7693e5
GalaxyGorilla <skattelmann@gmail.com>
2016-04-17 01:42:29 +0200

    first commit

---
+++ test/exometer_fetch_subscribe_mod.erl
@@ -0,0 +1,12 @@
+-module(exometer_fetch_subscribe_mod).
+-compile([export_all]).
+
+subscribe([metric, test1] = Metric, histogram) ->
+    Opts = [{key, <<"my_key1">>}],
+    {Metric, [min, max, median], Opts};
+subscribe([metric, test2] = Metric, histogram) ->
+    Opts1 = [{key, <<"my_key2">>}],
+    Opts2 = [{key, <<"my_key3">>}],
+    [{Metric, min, Opts1},
+     {Metric, [max, median], Opts2}];
+subscribe(_, _) -> [].
---
+++ test/exometer_fetch_test.erl
@@ -0,0 +1,25 @@
+-module(exometer_fetch_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+subscribtions_module_test() ->
+    Subscribers =  [ 
+                    {reporters, [{exometer_report_fetch, [{autosubscribe, true}, 
+                                                         {subscriptions_module, exometer_fetch_subscribe_mod}]}]}],
+    error_logger:tty(false),
+    application:set_env(lager, handlers, [{lager_console_backend, none}]),
+    application:set_env(exometer, report, Subscribers),
+    {ok, Apps} = application:ensure_all_started(exometer_fetch),
+
+    exometer:update_or_create([metric, test1], 1, histogram, []),
+    exometer:update_or_create([metric, test2], 2, histogram, []),
+
+    timer:sleep(100),
+    ?assertEqual(3, length(exometer_report:list_subscriptions(exometer_report_fetch))),
+
+    {ok, _} = exometer_fetch:fetch(<<"my_key1">>),
+    {ok, _} = exometer_fetch:fetch(<<"my_key1">>, min),
+    {ok, _} = exometer_fetch:fetch(<<"my">>),
+
+    [application:stop(App) || App <- Apps],
+    ok.
